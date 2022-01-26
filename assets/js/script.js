bd0ad7dcac78c02a4535bfe942e049ee22f59479
carolpang <carolpang17@gmail.com>
2022-01-25 16:35:39 -0800

    adding office hour on table

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,10 +8,31 @@
 //    console.log("you got it wrong")
 //  }
 
-var nowTime = "3:45pm"
+var nowTime = moment().format("h");
 
-$(".one-row").append(`<div class="time row-up">${nowTime}</div>`);
+function addRow(i) {
+  var regularHour;
+  if(i-4 > 0) {
+    regularHour = i-4 + " pm";
+  } else if(i+8 === 12) {
+    regularHour = i+8 + " pm";
+  } else {
+    regularHour = i+8 + " am";
+  }
+  $(".table").append(`<div class="one-row row${i}"></div>`);
 
-$(".one-row").append('<div class=" typing-box row-up"><div class="typing-space " ></div></div>');
+  $(`.row${i}`).append(`<div class="time row-up">${    regularHour
+}</div>`);
 
-$(".one-row").append('<div class="save-btn row-up"></div>');
+  $(`.row${i}`).append(
+    '<div class=" typing-box row-up"><div class="typing-space " ></div></div>'
+  );
+
+  $(`.row${i}`).append('<div class="save-btn row-up"></div>');
+}
+
+for (var i = 1; i < 10; i++) {
+  addRow(i);
+}
+
+//$('.save-btn').addClass('blueBack')
